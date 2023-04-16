7c989d774694d5991da2919aa4106d8e8e342a0e
Jorge Arnal <jorge.arnal08@gmail.com>
2023-04-16 19:40:19 -0300

    Actualizacion security

---
+++ miportfolio/src/main/java/com/miportfolio/miportfolio/Security/Dto/LoginUsuario.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.miportfolio.miportfolio.Security.Dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+
+
+public class LoginUsuario {
+    @NotBlank
+    private String nombreUsuario;
+    @NotBlank
+    private String contraseña; 
+    
+    //Getters and Setters
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+    
+}
